9b9b7d61a9e3e941335cabcbc784705c5d42f975
Ankit Kumar <56107403+Adoraemonn@users.noreply.github.com>
2024-01-20 09:41:37 +0530

    5th push

--- practice-3.sql
+++ practice-3.sql
@@ -3,3 +3,6 @@ select * from classicmodels.employees;
 select * from classicmodels.orderdetails order by quantityOrdered;
 
 select * from classicmodels.orders;
+
+select employeeNumber, reportsTo , employeeNumber + ifnull(reportsTo , 0) from classicmodels.employees;
+
